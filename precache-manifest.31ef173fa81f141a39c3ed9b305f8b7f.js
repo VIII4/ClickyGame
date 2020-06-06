737a2896862acb8475cf1ae286c1aa04dfe69d14
Lay84 <Iyan.laylor@gmail.com>
2020-06-05 18:45:14 -0700

    Updates

---
+++ precache-manifest.31ef173fa81f141a39c3ed9b305f8b7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "feeeb0923bd12dfb6e17b00677aecc0e",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "16914d15cb5b18b68de1",
+    "url": "/clickygame/static/css/main.d7c0789a.chunk.css"
+  },
+  {
+    "revision": "e50d4c9609983495334d",
+    "url": "/clickygame/static/js/2.26f1edb2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clickygame/static/js/2.26f1edb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16914d15cb5b18b68de1",
+    "url": "/clickygame/static/js/main.2238d23a.chunk.js"
+  },
+  {
+    "revision": "a221dadbdfa7f47a42e6",
+    "url": "/clickygame/static/js/runtime-main.c1c05c78.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clickygame/static/media/logo.5d5d9eef.svg"
+  }
+]);
